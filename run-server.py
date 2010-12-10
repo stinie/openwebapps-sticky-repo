6652f66ac11d42c1a18ea46887e42d2b6af54290
Ian Bicking <ianb@colorstudy.com>
2010-12-10 17:23:23 -0600

    Added a setup step for db setup, and added velruse as a submodule

--- run-server.py
+++ run-server.py
@@ -6,6 +6,7 @@ import sys
 here = os.path.dirname(os.path.abspath(__file__))
 silver = os.path.join(here, 'silver')
 site.addsitedir(os.path.join(silver, 'lib-python'))
+sys.path.insert(0, os.path.join(silver, 'velruse'))
 sys.path.insert(0, here)
 
 from paste.httpserver import serve
@@ -25,13 +26,18 @@ def make_app():
     return app
 
 
-def serve_app(port, database, config):
+def serve_app(port, database, config, setup):
     os.environ['CONFIG_MYSQL_SQLALCHEMY'] = database
     os.environ['DEBUG_STICKYREPO'] = '1'
     os.environ['SILVER_APP_CONFIG'] = config
     os.environ['TEMP'] = '/tmp'
-    app = make_app()
-    serve(app, port=port)
+    if setup:
+        data_app = sticky_make_app()
+        data_app.create_database(None)
+        print 'Database created'
+    else:
+        app = make_app()
+        serve(app, port=port)
 
 
 def main():
@@ -40,7 +46,7 @@ def main():
         usage='%prog --db=SOMETHING')
     parser.add_option(
         '-d', '--db',
-        help="Database to connect to (SQLAlchemy connection string)")
+        help="Database to connect to (SQLAlchemy connection string); you might want to try sqlite:///test.db")
     parser.add_option(
         '-p', '--port',
         default='8080',
@@ -49,11 +55,15 @@ def main():
         '--config',
         default=os.path.join(here, 'silver', 'default-config'),
         help='Location where the velruse.yaml or local.yaml config file is (for login API keys).  Clone git@github.com:mozilla/openwebapps-private.git to get our keys (in openwebapps-private/sync).')
+    parser.add_option(
+        '--setup',
+        action='store_true',
+        help="Setup the database (don't run the server)")
     options, args = parser.parse_args()
     port = int(options.port)
     if not options.db:
         parser.error('You must give --db')
-    serve_app(port, options.db, options.config)
+    serve_app(port, options.db, options.config, options.setup)
 
 
 if __name__ == '__main__':
